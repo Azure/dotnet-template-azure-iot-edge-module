2fbec8dd15ed81ea1d6e32ef86bc750fe0687e08
Summer <victoriasqq@gmail.com>
2017-11-13 16:58:20 +0800

    format cs template

--- content/dotnet-template-azure-iot-edge-module/CSharp/Program.cs
+++ content/dotnet-template-azure-iot-edge-module/CSharp/Program.cs
@@ -1,7 +1,7 @@
 namespace SampleModule
 {
     using System;
-	using System.IO;
+    using System.IO;
     using System.Collections.Generic;
     using System.Runtime.InteropServices;
     using System.Runtime.Loader;
