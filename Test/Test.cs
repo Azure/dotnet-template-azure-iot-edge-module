bf4f74b7ae633eff91122152a0635667ebc9128d
formulahendry <formulahendry@gmail.com>
2018-02-09 16:23:41 +0800

    Remove platform flag

--- Test/Test.cs
+++ Test/Test.cs
@@ -38,49 +38,23 @@ namespace Test
             this.fixture = fixture;
         }
 
-        public static Dictionary<string, List<string>> FlagFilesMapping = new Dictionary<string, List<string>>{
-            {
-                ArchLinux64, new List<string> {
-                    "Dockerfile",
-                    "Dockerfile.amd64.debug"
-                }
-            },
-            {
-                ArchWindowsNano, new List<string> {
-                    "Dockerfile"
-                }
-            }
-        };
-
-        private static string BeforeEach(string lang, string repository, bool linux64 = true, bool windowsNano = true, bool skipRestore = false)
+        private static string BeforeEach(string lang, string repository, bool skipRestore = false)
         {
             var scaffoldName = Path.GetRandomFileName().Replace(".", "").ToString();
-            var command = "new aziotedgemodule -n " + scaffoldName + " -lang " + lang + " -lx " + linux64 + " -wn " + windowsNano + " -s " + skipRestore + " -r " + repository;
+            var command = "new aziotedgemodule -n " + scaffoldName + " -lang " + lang  + " -s " + skipRestore + " -r " + repository;
             Process.Start("dotnet", command).WaitForExit();
             return scaffoldName;
         }
 
         [Theory]
-        [InlineData(CSharp, true, true, true)]
-        [InlineData(CSharp, true, true, false)]
-        [InlineData(CSharp, true, false, true)]
-        [InlineData(CSharp, true, false, false)]
-        [InlineData(CSharp, false, true, true)]
-        [InlineData(CSharp, false, true, false)]
-        [InlineData(CSharp, false, false, true)]
-        [InlineData(CSharp, false, false, false)]
-        [InlineData(FSharp, true, true, true)]
-        [InlineData(FSharp, true, true, false)]
-        [InlineData(FSharp, true, false, true)]
-        [InlineData(FSharp, true, false, false)]
-        [InlineData(FSharp, false, true, true)]
-        [InlineData(FSharp, false, true, false)]
-        [InlineData(FSharp, false, false, true)]
-        [InlineData(FSharp, false, false, false)]
-        public void TestArchitecture(string lang, bool linux64, bool windowsNano, bool skipRestore)
+        [InlineData(CSharp, true)]
+        [InlineData(CSharp, false)]
+        [InlineData(FSharp, true)]
+        [InlineData(FSharp, false)]
+        public void TestArchitecture(string lang, bool skipRestore)
         {
             var repository = "test.azurecr.io/test";
-            var scaffoldName = BeforeEach(lang, repository, linux64, windowsNano, skipRestore);
+            var scaffoldName = BeforeEach(lang, repository, skipRestore);
             var filesToCheck = new List<string> { ".gitignore", "module.json" };
 
             if (skipRestore)
@@ -99,16 +73,6 @@ namespace Test
             if (lang == FSharp)
             {
                 filesToCheck.AddRange(new List<string> { "Program.fs", scaffoldName + ".fsproj" });
-            }
-
-            if (linux64)
-            {
-                filesToCheck.AddRange(FlagFilesMapping[ArchLinux64]);
-
-            }
-            if (windowsNano)
-            {
-                filesToCheck.AddRange(FlagFilesMapping[ArchWindowsNano]);
             }
 
             foreach (var file in filesToCheck)
