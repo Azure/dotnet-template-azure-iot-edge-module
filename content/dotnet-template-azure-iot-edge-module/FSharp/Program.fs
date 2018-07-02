7dcea3c81e2796dc2eb2482896efe4ece329f2d3
William Berry <wberry@microsoft.com>
2018-07-02 16:00:25 -0700

    udpate F# module for production SDKs

--- content/dotnet-template-azure-iot-edge-module/FSharp/Program.fs
+++ content/dotnet-template-azure-iot-edge-module/FSharp/Program.fs
@@ -1,10 +1,8 @@
 ﻿namespace SampleModule
 
 open System
-open System.IO
 open System.Runtime.InteropServices
 open System.Runtime.Loader
-open System.Security.Cryptography.X509Certificates
 open System.Text
 open System.Threading
 open System.Threading.Tasks
@@ -20,8 +18,8 @@ module SampleModule =
     let PipeMessage (message:Message) (userContext:obj) =
         let counterValue = Interlocked.Increment(counter)
 
-        let deviceClient = userContext :?> DeviceClient
-        if (isNull(deviceClient)) then
+        let moduleClient = userContext :?> ModuleClient
+        if (isNull(moduleClient)) then
             raise (InvalidOperationException("UserContext doesn't contain " + "expected values"))
 
         let messageBytes = message.GetBytes()
@@ -34,7 +32,7 @@ module SampleModule =
             message.Properties 
             |> Seq.iter (fun prop -> pipeMessage.Properties.Add(prop.Key, prop.Value))
             
-            deviceClient.SendEventAsync("output1", pipeMessage) 
+            moduleClient.SendEventAsync("output1", pipeMessage) 
             |> Async.AwaitTask 
             |> Async.Start
 
@@ -43,52 +41,37 @@ module SampleModule =
         Task.FromResult (MessageResponse.Completed)
 
     let Init (connectionString:string) (bypassCertVerification:bool) =
-        printfn "Connection String %s" connectionString
+        async {
+            printfn "Connection String %s" connectionString
 
-        let mqttSetting = MqttTransportSettings(TransportType.Mqtt_Tcp_Only)
-        // During dev you might want to bypass the cert verification. 
-        // It is highly recommended to verify certs systematically in production
-        if bypassCertVerification then
-            mqttSetting.RemoteCertificateValidationCallback <- (fun _ _ _ _ -> true) 
-        let transportSettings = mqttSetting :> ITransportSettings
-        let settings = [|transportSettings|] 
+            let mqttSetting = MqttTransportSettings(TransportType.Mqtt_Tcp_Only)
+            // During dev you might want to bypass the cert verification. 
+            // It is highly recommended to verify certs systematically in production
+            if bypassCertVerification then
+                mqttSetting.RemoteCertificateValidationCallback <- (fun _ _ _ _ -> true) 
+            let transportSettings = mqttSetting :> ITransportSettings
+            let settings = [|transportSettings|] 
 
-        // Open a connection to the Edge runtime
-        let ioTHubModuleClient = 
-            DeviceClient.CreateFromConnectionString(connectionString, settings)
+            // Open a connection to the Edge runtime
+            let! ioTHubModuleClient = 
+                ModuleClient.CreateFromEnvironmentAsync(settings)
+                |> Async.AwaitTask 
+                ////|> Async.
+                //|> Async.Start
 
-        ioTHubModuleClient.OpenAsync() 
-        |> Async.AwaitTask 
-        |> Async.Start
-        
-        printfn "IoT Hub module client initialized."
+            ioTHubModuleClient.OpenAsync() 
+            |> Async.AwaitTask 
+            |> Async.Start
+            
+            printfn "IoT Hub module client initialized."
 
-        // Register callback to be called when a message is received by the module
-        let pipeMessageHandler = new MessageHandler(PipeMessage)
+            // Register callback to be called when a message is received by the module
+            let pipeMessageHandler = new MessageHandler(PipeMessage)
 
-        ioTHubModuleClient.SetInputMessageHandlerAsync("input1", pipeMessageHandler, ioTHubModuleClient)
-        |> Async.AwaitTask 
-        |> Async.Start
-        
-
-    let InstallCert () =
-        let certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
-        
-        if (String.IsNullOrWhiteSpace(certPath)) then
-            // We cannot proceed further without a proper cert file
-            printfn "Missing path to certificate collection file: %s" certPath
-            raise (InvalidOperationException("Missing path to certificate file."))
-        elif (not (File.Exists(certPath))) then
-            // We cannot proceed further without a proper cert file
-            printfn "Missing path to certificate collection file: %s" certPath
-            raise (InvalidOperationException("Missing certificate file."))
-        else
-            let store = new X509Store(StoreName.Root, StoreLocation.CurrentUser)
-            store.Open(OpenFlags.ReadWrite)
-            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)))
-            Console.WriteLine("Added Cert: " + certPath)
-            store.Close()
-        
+            ioTHubModuleClient.SetInputMessageHandlerAsync("input1", pipeMessageHandler, ioTHubModuleClient)
+            |> Async.AwaitTask 
+            |> Async.Start
+        }
 
     let WhenCancelled (cancellationToken:CancellationToken) : Task<bool> =
             let tcs = new TaskCompletionSource<bool>()
@@ -104,8 +87,7 @@ module SampleModule =
 
         // Cert verification is not yet fully functional when using Windows OS for the container
         let bypassCertVerification = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-        if (not bypassCertVerification) then InstallCert ()
-        Init connectionString bypassCertVerification
+        Init connectionString bypassCertVerification |> Async.RunSynchronously
 
         // Wait until the app unloads or is cancelled
         let cts = new CancellationTokenSource()
